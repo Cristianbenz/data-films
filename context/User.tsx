7f4a2dbeb87f9b8e876e579fff70aef46e0d7d72
Cristianbenz <contactocbenitez@gmail.com>
2023-01-04 21:16:54 -0300

    final version

--- context/User.tsx
+++ context/User.tsx
@@ -1,21 +1,33 @@
-import { createContext, ReactNode, useState } from 'react'
+import { createContext, ReactNode, useEffect, useState } from 'react'
+import { Cookies } from 'react-cookie'
+import { jwtVerify } from 'jose'
 
 export const userContext = createContext<any>({})
 
-function UserContextProvider({children}: {children: ReactNode}) {
+function UserContextProvider({ children }: { children: ReactNode }) {
   const [token, setToken] = useState<string | undefined>(undefined)
-
+  const cookies = new Cookies()
   function signUser(token: string) {
     setToken(token)
   }
 
+  useEffect(() => {
+    const token = cookies.get('authToken')
+    token && jwtVerify(token, new TextEncoder().encode(process.env.NEXT_PUBLIC_JWT_SECRET))
+      .then(res => {
+        if (res) {
+          setToken(token)
+        }
+      })
+  }, [])
+
   return (
-  <userContext.Provider value={{
-    token,
-    signUser
-  }}>
-    {children}
-  </userContext.Provider>
+    <userContext.Provider value={{
+      token,
+      signUser
+    }}>
+      {children}
+    </userContext.Provider>
   )
 }
 
